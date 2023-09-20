bfb2c98ae22e5b1fa0957e5898925a2a4835b6f5
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-19 21:42:13 -0700

    added create thought logic

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -36,3 +36,22 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
+
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
