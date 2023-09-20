957f30465221f4f206f8880b9824d6ae6678449a
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-19 21:28:46 -0700

    added logic to find single thought by id

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -15,3 +15,24 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
+
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v',
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thoughts found with that id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
