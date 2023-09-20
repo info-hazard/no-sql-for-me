e29dd12b6c23d1d56d846dfcd26dea69f516979c
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-20 01:54:22 -0700

    added user routes and exported to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thoughtroutes');
+router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 module.exports = router;
