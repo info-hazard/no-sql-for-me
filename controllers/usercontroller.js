2e436d51577da32700a0a338f7f7cd704c209e74
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-19 23:37:31 -0700

    added find all and find single User by ID fuction

---
+++ controllers/usercontroller.js
@@ -0,0 +1,36 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
